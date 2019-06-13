be31146cfa0485d494f0b8813327eec89a19af26
GUILHERMECAMPOSFERRE <GUILHERMECAMPOSFERRE@DESKTOP-DG6RBVE.lan>
2019-06-13 01:13:33 -0300

    Commit Inicial

--- src/boundary/AnimalBoundary.java
+++ src/boundary/AnimalBoundary.java
@@ -1,93 +1,120 @@
 package boundary;
 
-import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import com.sun.javafx.css.Stylesheet;
+import javax.swing.JOptionPane;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import control.AnimalControl;
 import entity.Animal;
 import javafx.application.Application;
+import javafx.beans.property.ReadOnlyIntegerWrapper;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class AnimalBoundary implements EventHandler<ActionEvent> {
+public class AnimalBoundary extends Application implements EventHandler<ActionEvent> {
 	private TextField txtId = new TextField();
 	private TextField txtNome = new TextField();
-	private TextField txtData = new TextField();
+	private TextField txtNasc = new TextField();
 	private TextField txtPeso = new TextField();
 	private Button btnAdd = new Button("Adicionar");
 	private Button btnSch = new Button("Pesquisar");
 
 	private AnimalControl control = new AnimalControl();
-	private TableView<Animal> tableView = new TableView<>(); 
+	private TableView<Animal> tableView = new TableView<>();
 	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
-	
+
 	public void start(Stage stage) throws Exception {
 		VBox box = new VBox();
 		GridPane grid = new GridPane();
-		Scene scene = new Scene(box, 300, 300);
+		Scene scene = new Scene(box, 500, 300);
 		box.getChildren().addAll(grid, tableView);
-		
+
 		createTableColumns();
-		
+		txtId.setText("");
+		txtNome.setText("");
+		txtNasc.setText("");
+		txtPeso.setText("");
 		grid.add(new Label("Id"), 0, 0);
 		grid.add(txtId, 1, 0);
 		grid.add(new Label("Nome do Animal"), 0, 1);
 		grid.add(txtNome, 1, 1);
 		grid.add(new Label("Data de Nascimento"), 0, 2);
-		grid.add(txtData, 1, 2);
+		grid.add(txtNasc, 1, 2);
 		grid.add(new Label("Peso"), 0, 3);
 		grid.add(txtPeso, 1, 3);
 		grid.add(btnAdd, 0, 6);
 		grid.add(btnSch, 1, 6);
-		
+		btnAdd.setPrefWidth(150);
+		btnAdd.setStyle("-fx-border-color:#5173ee");
+		btnSch.setPrefWidth(150);
+		btnSch.setStyle("-fx-border-color:#5173ee");
 		btnAdd.addEventFilter(ActionEvent.ACTION, this);
 		btnSch.addEventFilter(ActionEvent.ACTION, this);
-		
-		
+
 		stage.setScene(scene);
-		stage.setTitle("Gestão de Pizzas");
+		stage.setTitle("Registro de Animais do Petshop");
 		stage.show();
-}
+	}
+
 	public void handle(ActionEvent event) {
 		if (event.getTarget() == btnAdd) {
-			Animal a = boundaryToAnimal();
-			control.adicionar(a);
-			animalToBoundary(new Animal());
+			if (!txtId.getText().equals("") && !txtNome.getText().equals("") && !txtNasc.getText().equals("")
+					&& !txtPeso.getText().equals("")) {
+				Animal a = boundaryToAnimal();
+				control.adicionar(a);
+				animalToBoundary(a);
+			} else {
+				JOptionPane.showMessageDialog(null,
+						"Não será possível fazer o registro, pois faltam informações a serem preenchidas");
+			}
 		} else if (event.getTarget() == btnSch) {
-			control.pesquisarPorNome(txtNome.getText());
+			if (!txtNome.getText().equals("")) {
+				control.pesquisarPorNome(txtNome.getText());
+			} else {
+				JOptionPane.showMessageDialog(null,
+						"Não será possível realizar a pesquisa, pois faltam informações a serem preenchidas");
+			}
 		}
 	}
 
 	private void animalToBoundary(Animal a) {
 		txtId.setText(String.valueOf(a.getId()));
 		txtNome.setText(a.getNome());
-		txtData.setText(sdf.format(a.getNascimento()));
+		String data = sdf.format(a.getNascimento());
+		txtNasc.setText(data);
+		System.out.println(data);
 		txtPeso.setText(String.format("%6.2f", a.getPeso()));
 	}
 
 	private Animal boundaryToAnimal() {
 		Animal a = new Animal();
 		a.setNome(txtNome.getText());
-		p.set(txtIngredientes.getText());
-		p.setTamanho(cmbTamanho.getValue());
 		try {
-			p.setId(Long.parseLong(txtId.getText()));
-			p.setPreco(Float.parseFloat(txtPreco.getText()));
-			Date d = sdf.parse(txtFabricacao.getText());
-			p.setFabricacao(d);
+			a.setId(Integer.parseInt(txtId.getText()));
+			a.setPeso(Float.parseFloat(txtPeso.getText()));
+			Date d = sdf.parse(txtNasc.getText());
+			System.out.println(d);
+			a.setNascimento(d);
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
 		} catch (ParseException e) {
@@ -98,25 +125,24 @@ public class AnimalBoundary implements EventHandler<ActionEvent> {
 
 	private void createTableColumns() {
 		tableView.setItems(control.getDataList());
-		tableView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Pizza>() {
+		tableView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Animal>() {
 			@Override
-			public void changed(ObservableValue<? extends Pizza> p, Animal a1, Animal a2) {
-				animalToBoundary(p2);
+			public void changed(ObservableValue<? extends Animal> a, Animal a1, Animal a2) {
+				animalToBoundary(a2);
 			}
 		});
-		TableColumn<Pizza, Number> idColumn = new TableColumn<>("Id");
-		idColumn.setCellValueFactory(item -> new ReadOnlyLongWrapper(item.getValue().getId()));
+		TableColumn<Animal, Number> idColumn = new TableColumn<>("Id");
+		idColumn.setCellValueFactory(item -> new ReadOnlyIntegerWrapper(item.getValue().getId()));
 
-		TableColumn<Pizza, String> saborColumn = new TableColumn<>("Sabor");
-		saborColumn.setCellValueFactory(item -> new ReadOnlyStringWrapper(item.getValue().getSabor()));
+		TableColumn<Animal, String> nomeColumn = new TableColumn<>("Nome");
+		nomeColumn.setCellValueFactory(item -> new ReadOnlyStringWrapper(item.getValue().getNome()));
 
-		TableColumn<Pizza, Double> precoColumn = new TableColumn<>("Preço");
-		precoColumn.setCellValueFactory(new PropertyValueFactory<Pizza, Double>("preco"));
+		TableColumn<Animal, String> dataColumn = new TableColumn<>("Fabricação");
+		dataColumn.setCellValueFactory(item -> new ReadOnlyStringWrapper(sdf.format(item.getValue().getNascimento())));
 
-		TableColumn<Pizza, String> fabricColumn = new TableColumn<>("Fabricação");
-		fabricColumn
-				.setCellValueFactory(item -> new ReadOnlyStringWrapper(sdf.format(item.getValue().getFabricacao())));
+		TableColumn<Animal, Double> pesoColumn = new TableColumn<>("Peso");
+		pesoColumn.setCellValueFactory(new PropertyValueFactory<Animal, Double>("peso"));
 
-		tableView.getColumns().addAll(idColumn, saborColumn, precoColumn, fabricColumn);
+		tableView.getColumns().addAll(idColumn, nomeColumn, dataColumn, pesoColumn);
 	}
 }
--- src/control/AnimalControl.java
+++ src/control/AnimalControl.java
@@ -10,19 +10,30 @@ import javafx.collections.ObservableList;
 public class AnimalControl {
 	private List<Animal> animais = new ArrayList<Animal>();
 	private ObservableList<Animal> dataList = FXCollections.observableArrayList();
-	
+
 	public void adicionar(Animal a) {
 		animais.add(a);
 		dataList.clear();
 		dataList.addAll(animais);
 	}
-	
+
 	public Animal pesquisarPorNome(String nome) {
+		dataList.clear();
 		for (Animal a : animais) {
 			if (a.getNome().contains(nome)) {
+				dataList.add(a);
 				return a;
 			}
 		}
+		dataList.addAll(animais);
 		return null;
+	}
+
+	public ObservableList<Animal> getDataList() {
+		return dataList;
+	}
+
+	public void setDataList(ObservableList<Animal> dataList) {
+		this.dataList = dataList;
 	}
 }
